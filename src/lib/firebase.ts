6ee53bc109c5c5bc45bc5a7be211839aa54951a9
Oscar <oscarrc.web@gmail.com>
2025-06-29 13:29:25 +0200

    add firebase secrets

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,13 +10,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNTYgOa0N-WxaiD_O1UozFVtReIZxcoTc",
-  authDomain: "car-management-4fc0a.firebaseapp.com",
-  projectId: "car-management-4fc0a",
-  storageBucket: "car-management-4fc0a.firebasestorage.app",
-  messagingSenderId: "160521121979",
-  appId: "1:160521121979:web:d01f214b888936b619b99e",
-  measurementId: "G-Z2Z7ZM4WF8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
