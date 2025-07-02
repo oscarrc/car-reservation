aa7a66ed6b68ea3f3e95b592d38a911596b8b77e
Oscar <oscarrc.web@gmail.com>
2025-07-02 17:03:47 +0200

    fix missing strings and change unsuspend text color

--- src/components/users/users-columns.tsx
+++ src/components/users/users-columns.tsx
@@ -175,7 +175,7 @@ export const createColumns = ({
                     ? onUnsuspendUser && (
                         <DropdownMenuItem
                           onClick={() => onUnsuspendUser(user)}
-                          className="cursor-pointer text-green-600"
+                          className="cursor-pointer"
                         >
                           <UserCheck className="mr-2 h-4 w-4" />
                           {t("users.unsuspendUser")}
