17acb80ce8b3b39cb134043b7ba9c0ff036ddaf5
Oscar <oscarrc.web@gmail.com>
2025-07-03 19:52:08 +0200

    fix // eslint-disable-next-line react-refresh/only-export-components

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -41,12 +41,12 @@ export default function SidebarLayout({ config }: { config: SidebarConfig }) {
         }
       >
         <AppSidebar config={updatedConfig} variant="inset" />
-        <SidebarInset className="overflow-hidden h-[calc(100vh-1rem)] my-4 mr-4 flex flex-col">
+        <SidebarInset className="overflow-hidden h-[calc(100vh-1rem)] flex flex-col">
           {/* Fixed Header at top */}
           <div className="flex-shrink-0 bg-background border-b">
             <SiteHeader companyName={companyName} homePageUrl={homePageUrl} />
           </div>
-          
+
           {/* Scrollable Content Area */}
           <div className="flex-1 overflow-y-auto">
             <div className="@container/main flex flex-1 flex-col gap-2">
